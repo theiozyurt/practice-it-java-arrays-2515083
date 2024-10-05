ee6c10163d5109a93cd4ed0fa24543b032804cf6
İsmail Özyurt <150421194+theiozyurt@users.noreply.github.com>
2024-10-05 16:54:11 +0000

    Working With Array Data

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
   
   public static void main(String[] args) {
-    
+    double[] lotteryNums  = {30,29,28,27,26};
+    System.out.println(lotteryNums[2]);
+    lotteryNums[2]=54;
+    System.out.println(lotteryNums[2]);
+
   }
 }
